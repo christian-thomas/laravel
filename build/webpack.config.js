c16553512a316caf9edef27732fd89727e8783b8
Adam Gilleard <adam@gilleard.co.uk>
2020-04-09 16:35:41 +0100

    Add babel transform-runtime for async functionality

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -14,11 +14,22 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
+						cacheDirectory: true,
 						presets: [
 							[
 								'@babel/preset-env',
 								{
 									// https://babeljs.io/docs/en/babel-preset-env#options
+								},
+							],
+						],
+            plugins: [
+							// '@babel/plugin-syntax-dynamic-import',
+							// '@babel/plugin-proposal-object-rest-spread',
+							[
+								'@babel/plugin-transform-runtime',
+								{
+									// https://babeljs.io/docs/en/next/babel-plugin-transform-runtime.html#options
 								},
 							],
 						],
