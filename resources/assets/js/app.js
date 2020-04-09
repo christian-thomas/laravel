e09f2bebb2e7cbccf743a4005970375b42a59275
Adam Gilleard <adam@gilleard.co.uk>
2020-04-09 16:44:26 +0100

    Reset polyfill.io usage

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -74,5 +74,3 @@ console.log(z); // { a: 3, b: 4 }
 
 let n = { x, y, ...z };
 console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
-
-window.alert(159);
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -23,7 +23,15 @@
 	</div>
 
 	@section('app:after')
-		<script src="https://polyfill.io/v3/polyfill.min.js?features={{ implode('%2C', ['Array.from', 'Promise', 'IntersectionObserver', 'Element.prototype.matches', 'Element.prototype.classList', 'Array.prototype.includes', 'Array.prototype.find']) }}"></script>
+		<script src="https://polyfill.io/v3/polyfill.min.js?features={{ implode('%2C', [
+			// 'Array.from',
+			'Promise',
+			// 'IntersectionObserver',
+			// 'Element.prototype.matches',
+			// 'Element.prototype.classList',
+			// 'Array.prototype.includes',
+			// 'Array.prototype.find',
+		]) }}"></script>
 		<script src="{{ mix('/compiled/js/app.js') }}" async></script>
 	@show
 </body>
