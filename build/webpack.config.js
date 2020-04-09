47cee9895d528c89848ad04c16ea945092f5bf79
Adam Gilleard <adam@gilleard.co.uk>
2020-04-09 16:08:29 +0100

    Add babel

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,4 +6,25 @@ module.exports = {
 		filename: 'app.js',
 		path: path.resolve(__dirname, '../public/compiled/js'),
 	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									// https://babeljs.io/docs/en/babel-preset-env#options
+								},
+							],
+						],
+					},
+				},
+			},
+		],
+	},
 };
