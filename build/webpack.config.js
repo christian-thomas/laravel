4248aedae2f64e93bfc15333d3fa64292befb333
Adam Gilleard <adam@gilleard.co.uk>
2020-04-14 16:34:06 +0100

    Enable source maps when not in prod mode

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,85 +1,90 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-module.exports = {
-	context: path.resolve(__dirname, '../resources/assets'),
-	entry: {
-		app: './js/app.js',
-	},
-	output: {
-    filename: 'js/[name].js',
-		path: path.resolve(__dirname, '../public/compiled/'),
-		publicPath: '/compiled/',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: [
-							[
-								'@babel/preset-env',
-								{
-									// https://babeljs.io/docs/en/babel-preset-env#options
-								},
-							],
-						],
-						plugins: [
-							// '@babel/plugin-syntax-dynamic-import',
-							// '@babel/plugin-proposal-object-rest-spread',
-							[
-								'@babel/plugin-transform-runtime',
-								{
-									// https://babeljs.io/docs/en/next/babel-plugin-transform-runtime.html#options
-								},
-							],
-						],
-					},
+module.exports = (env, { mode }) => {
+	const prod = mode === 'production';
+
+	return {
+		context: path.resolve(__dirname, '../resources/assets'),
+		entry: {
+			app: './js/app.js',
+		},
+		output: {
+			filename: 'js/[name].js',
+			path: path.resolve(__dirname, '../public/compiled/'),
+			publicPath: '/compiled/',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
 				},
-			},
-			{
-				// TODO: check w/ fonts
-				test: /\.(jpe?g|png|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
 						options: {
-							context: 'resources/assets',
-							esModule: false,
-							name: '[path][name].[ext]',
+							cacheDirectory: true,
+							presets: [
+								[
+									'@babel/preset-env',
+									{
+										// https://babeljs.io/docs/en/babel-preset-env#options
+									},
+								],
+							],
+							plugins: [
+								// '@babel/plugin-syntax-dynamic-import',
+								// '@babel/plugin-proposal-object-rest-spread',
+								[
+									'@babel/plugin-transform-runtime',
+									{
+										// https://babeljs.io/docs/en/next/babel-plugin-transform-runtime.html#options
+									},
+								],
+							],
 						},
 					},
-				],
-			},
+				},
+				{
+					// TODO: check w/ fonts
+					test: /\.(jpe?g|png|svg)$/i,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								context: 'resources/assets',
+								esModule: false,
+								name: '[path][name].[ext]',
+							},
+						},
+					],
+				},
+			],
+		},
+		plugins: [
+			new VueLoaderPlugin(),
 		],
-	},
-	plugins: [
-		new VueLoaderPlugin(),
-	],
-	resolve: {
-		alias: {
-			// 'vue$': 'vue/dist/vue.common.js',
-			'vue$': 'vue/dist/vue.esm.js',
-			'@': path.resolve(__dirname, '../resources/assets/'),
+		resolve: {
+			alias: {
+				// 'vue$': 'vue/dist/vue.common.js',
+				'vue$': 'vue/dist/vue.esm.js',
+				'@': path.resolve(__dirname, '../resources/assets/'),
+			},
 		},
-	},
-	devServer: {
-		host: '0.0.0.0',
-		port: 5757,
-		public: 'localhost:5757',
-		publicPath: '/compiled/',
-		proxy: {
-			context: ['**', '!/compiled/**'],
-			target: 'http://web:80',
+		devtool: prod ? false : 'cheap-eval-source-map',
+		devServer: {
+			host: '0.0.0.0',
+			port: 5757,
+			public: 'localhost:5757',
+			publicPath: '/compiled/',
+			proxy: {
+				context: ['**', '!/compiled/**'],
+				target: 'http://web:80',
+			},
+			stats: 'minimal',
 		},
-		stats: 'minimal',
-	},
+	};
 };
