67359d5f2bb7b87c14dc2468e12a8106ed9e3ecd
Adam Gilleard <adam@gilleard.co.uk>
2020-04-15 13:41:42 +0100

    Switch to native hashing

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-const crypto = require('crypto');
 const path = require('path');
 
 const ManifestPlugin = require('webpack-manifest-plugin');
@@ -8,25 +6,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = (env, { mode }) => {
 	const prod = mode === 'production';
 
-	const hash = (file) => {
-		if (!prod) {
-			return file;
-		}
-
-		const absolutePath = path.resolve(__dirname, `../public${file}`);
-
-		const data = fs.readFileSync(absolutePath, 'utf8');
-
-		return crypto.createHash('sha1').update(data).digest('hex').substr(0, 20);
-	};
-
 	return {
 		context: path.resolve(__dirname, '../resources/assets'),
 		entry: {
 			'js/app': './js/app.js',
 		},
 		output: {
-			filename: '[name].js',
+			filename: prod ? '[name].[contenthash].js' : '[name].js',
 			path: path.resolve(__dirname, '../public/compiled/'),
 			publicPath: '/compiled/',
 		},
@@ -97,10 +83,6 @@ module.exports = (env, { mode }) => {
 			new ManifestPlugin({
 				basePath: '/compiled/',
 				fileName: '../mix-manifest.json',
-				generate: (seed, files, entrypoints) => files.reduce((manifest, { name, path }) => ({
-					...manifest,
-					[name]: prod ? `${path}?${hash(name)}` : path,
-				}), seed),
 			}),
 			new VueLoaderPlugin(),
 		],
