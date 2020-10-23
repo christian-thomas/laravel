0ed84ef39beb7465a190be6a71727d703e29122d
Adam Gilleard <adam@gilleard.co.uk>
2020-10-23 15:53:58 +0100

    Temporarily add demo usage

--- resources/views/app/home/home.blade.php
+++ resources/views/app/home/home.blade.php
@@ -1,4 +1,43 @@
 @extends('layouts/app')
 
 @section('content')
+	<div class="my-12 space-y-12 max-w-sm">
+		{{-- Default --}}
+		<placeholder
+			src="https://placehold.it/1600x900"
+			alt=""
+		>
+		</placeholder>
+
+		{{-- Ratio override --}}
+		<placeholder
+			src="https://placehold.it/200x200"
+			alt=""
+			ratio-class="pt-full"
+		>
+		</placeholder>
+
+		{{-- Custom slot --}}
+		<placeholder>
+			<div
+				slot-scope="{ placeholderClass }"
+				:class="placeholderClass"
+				class="flex items-end bg-red"
+			>
+				Anim labore qui
+			</div>
+		</placeholder>
+
+		{{-- Roll your own classes --}}
+		<placeholder>
+			<div
+				class="
+					absolute top-0 left-0
+					flex items-end bg-blue
+				"
+			>
+				Anim labore qui
+			</div>
+		</placeholder>
+	</div>
 @endsection
