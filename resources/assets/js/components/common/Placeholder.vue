fb574b97291d1bc491c5acc7797da3d3e2f8c010
Adam Gilleard <adam@gilleard.co.uk>
2020-10-23 15:53:44 +0100

    Add proposed component

--- resources/assets/js/components/common/Placeholder.vue
+++ resources/assets/js/components/common/Placeholder.vue
@@ -1,5 +1,29 @@
 <template>
-	<div class="e-placeholder">
-		<slot />
+	<div
+		class="relative w-full"
+		:class="$props.ratioClass"
+	>
+		<slot :placeholder-class="cPlaceholderClass">
+			<img :class="cPlaceholderClass" v-bind="$attrs">
+		</slot>
 	</div>
 </template>
+
+<script>
+	export default {
+		inheritAttrs: false,
+
+		props: {
+			ratioClass: {
+				type: String,
+				default: 'pt-9/16',
+			},
+		},
+
+		computed: {
+			cPlaceholderClass() {
+				return 'absolute top-0 left-0 w-full h-full';
+			},
+		},
+	};
+</script>
